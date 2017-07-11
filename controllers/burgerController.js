cbf5986d76ef955cc560cbfb8a8a3bbc5c207e50
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-11 14:53:23 -0400

    Homework

---
+++ controllers/burgerController.js
@@ -0,0 +1,39 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/", function(req, res) {
+	burger.create([
+		"name", "eaten"
+	], [
+		req.body.name, req.body.sleepy
+	], function() {
+		res.redirect("/");
+	});
+});
+
+router.put("/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+
+	console.log("condition", condition);
+
+	burger.update({
+		eaten: req.body.eaten
+	}, condition, function() {
+		res.redirect("/");
+	});
+});
+
+module.exports = router;
