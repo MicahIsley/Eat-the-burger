21089b3a9e1f1716a76cbdd14a96e591352b1897
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-18 21:11:24 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 var mysql = require("mysql");
 var Sequelize = require("sequelize");
 var connection;
-
+var JAWSDB_URL = {
+		host: "a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+		user: "pdd2hkivnvqewtwg	",
+		password: "c0p2sj9wduotgt27",
+		database: "bybn2hmhv4qiag0f"
+	}
+	
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
