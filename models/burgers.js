da96b2a8a36c253d24d3afeaaef183117cb1c7f7
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-18 21:47:13 -0400

    cli

---
+++ models/burgers.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Burgers = sequelize.define('Burgers', {
+    name: DataTypes.STRING,
+    eaten: DataTypes.BOOLEAN
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Burgers;
+};
