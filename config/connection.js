b922d7b2becaef35aa46f825254a2508e46f1c41
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-18 21:34:21 -0400

    connect

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,20 @@
 var mysql = require("mysql");
 var Sequelize = require("sequelize");
-var connection;
 var sequelize;
+
+
 var JAWSDB_URL = {
 		host: "a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 		user: "pdd2hkivnvqewtwg	",
 		password: "c0p2sj9wduotgt27",
 		database: "bybn2hmhv4qiag0f",
 		dialect: "mysql"
-	}
+}
 
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-	sequelize = new Sequelize(JAWSDB_URL);
+	sequelize = new Sequelize(JAWSDB_URL.database, JAWSDB_URL.user, JAWSDB_URL.password);
 } else {
-	connection = mysql.createConnection({
-		host: "localhost",
-		user: "root",
-		password: "Password123",
-		database: "burger_db"
-	});
 	sequelize = new Sequelize("burger_db", "root", "Password123", {dialect: "mysql"});
 };
 
