4ab3b7410a464139edddffe0d6ac25ff025899e0
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-07-18 21:28:02 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,14 @@ var JAWSDB_URL = {
 		host: "a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 		user: "pdd2hkivnvqewtwg	",
 		password: "c0p2sj9wduotgt27",
-		database: "bybn2hmhv4qiag0f"
+		database: "bybn2hmhv4qiag0f",
+		dialect: "mysql"
 	}
 
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-	sequelize = new Sequelize(JAWSDB_URL, {dialect: "mysql"});
+	sequelize = new Sequelize(JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
